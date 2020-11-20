727e9d7077031e033aebd2d8cd09cd60948f443b
DonColon <dardan.rrafshi@gmail.com>
2020-11-20 08:00:16 +0100

    Exported modules

---
+++ src/index.js
@@ -0,0 +1,87 @@
+
+export {
+    addMilliseconds,
+    addSeconds,
+    addMinutes,
+    addHours,
+    addDays,
+    addWeeks,
+    addMonths,
+    addQuarters,
+    addYears
+
+} from './modules/add';
+
+export {
+    subtractMilliseconds,
+    subtractSeconds,
+    subtractMinutes,
+    subtractHours,
+    subtractDays,
+    subtractWeeks,
+    subtractMonths,
+    subtractQuarters,
+    subtractYears
+
+} from './modules/subtract';
+
+export {
+    differenceInMilliseconds,
+    differenceInSeconds,
+    differenceInMinutes,
+    differenceInHours,
+    differenceInDays,
+    differenceInWeeks,
+    differenceInMonths,
+    differenceInQuarters,
+    differenceInYears
+
+} from './modules/difference';
+
+export {
+    min,
+    max,
+    isEqual,
+    isSameSecond,
+    isSameMinute,
+    isSameHour,
+    isSameDay,
+    isSameWeekday,
+    isSameWeek,
+    isSameMonth,
+    isSameQuarter,
+    isSameYear,
+    isCurrentSecond,
+    isCurrentMinute,
+    isCurrentHour,
+    isCurrentDay,
+    isCurrentWeekday,
+    isCurrentWeek,
+    isCurrentMonth,
+    isCurrentQuarter,
+    isCurrentYear,
+    isBefore,
+    isPast,
+    isYesterday,
+    isAfter,
+    isFuture,
+    isTomorrow,
+    isBetween
+
+} from './modules/compare';
+
+export {
+    getDayOfYear,
+    getWeekOfYear,
+    getQuarterOfYear,
+    isDate,
+    isLeapYear,
+    isMonday,
+    isTuesday,
+    isWednesday,
+    isThursday,
+    isFriday,
+    isSaturday,
+    isSunday
+
+} from './modules/calendar';

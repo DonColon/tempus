327878a687dd87b1d0569b65aaf9103a0b00563d
DonColon <dardan.rrafshi@gmail.com>
2020-11-20 07:59:30 +0100

    Implemented difference functions

---
+++ src/modules/difference.js
@@ -0,0 +1,48 @@
+
+import { getQuarterOfYear } from './calendar';
+
+
+export function differenceInMilliseconds(value, other) {
+    return Math.abs(value.getTime() - other.getTime());
+}
+
+export function differenceInSeconds(value, other) {
+    const milliseconds = differenceInMilliseconds(value, other);
+    return milliseconds / 1000;
+}
+
+export function differenceInMinutes(value, other) {
+    const seconds = differenceInSeconds(value, other);
+    return seconds / 60;
+}
+
+export function differenceInHours(value, other) {
+    const minutes = differenceInMinutes(value, other);
+    return minutes / 60;
+}
+
+export function differenceInDays(value, other) {
+    const hours = differenceInHours(value, other);
+    return hours / 24;
+}
+
+export function differenceInWeeks(value, other) {
+    const days = differenceInDays(value, other);
+    return days / 7;
+}
+
+export function differenceInMonths(value, other) {
+    const years = differenceInYears(value, other);
+    const months = Math.abs(value.getMonth() - other.getMonth());
+    return years * 12 + months;
+}
+
+export function differenceInQuarters(value, other) {
+    const years = differenceInYears(value, other);
+    const quarters = Math.abs(getQuarterOfYear(value) - getQuarterOfYear(other));
+    return years * 4 + quarters;
+}
+
+export function differenceInYears(value, other) {
+    return Math.abs(value.getFullYear() - other.getFullYear());
+}

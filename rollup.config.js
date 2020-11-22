fcd439692b92ed034d263f5f7900ffce4829a7e9
DonColon <dardan.rrafshi@gmail.com>
2020-11-22 12:25:41 +0100

    Removed unused rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { commonjs } from '@rollup/plugin-commonjs';
 
 import pkg from './package.json';
 
